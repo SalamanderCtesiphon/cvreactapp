79dcf7a389499ce69fb21cdcd6e712732c8bafd3
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-14 18:18:19 -0500

    I implemented a modal for the user input

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-
+import { useState } from 'react'
 import './App.css'
 import EducationalInfo from './components/EducationalInfo'
 import GeneralInfo from './components/GeneralInfo'
 import PracticalExperience from './components/PracticalExperience'
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleShowModal = () => {
+    setShowModal(!showModal);
+  };
   
+  const [resume, setResume] = useState({
+    firstName: "FirstName",
+    lastName: "LastName"
+  })
+  const [firstName, setFirstName] = useState('')
 
   return (
     <>
-    <GeneralInfo />
+    <div className="App">
+      <GeneralInfo 
+        firstName={firstName}
+        show={showModal} 
+        onCloseButtonClick={toggleShowModal} 
+      />
+      <button 
+        className="blue-button" 
+        onClick={toggleShowModal}
+      >
+        Open Modal
+      </button>
+    </div>
     <EducationalInfo />
     <PracticalExperience />
     </>
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,24 +1,34 @@
-import { useState } from 'react'
 
-function GeneralInfo() {
 
-  const [resume, setResume] = useState({
-    firstName: "FirstName",
-    lastName: "LastName"
-  })
-  const [firstName, setFirstName] = useState('')
+function GeneralInfo({ firstName, show, onCloseButtonClick }) {
+  if (!show) {
+    return null;
+  }
+ 
 
   return (
     <>
-      <input 
-      type="text" 
-      name="firstName" 
-      id="firstName" 
-      value={firstName}
-      placeholder='enter your first name'
-      onChange={(e) => setFirstName(e.target.value)}
-      />
-      {firstName}
+      <div className="modal-wrapper">
+      <div className="modal">
+        <div className="body">
+          Click on the close button to close the modal.
+        </div>
+        <input 
+          type="text" 
+          name="firstName" 
+          id="firstName" 
+          value={firstName}
+          placeholder='enter your first name'
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        {firstName}
+        <div className="footer">
+          <button onClick={onCloseButtonClick}>Close Modal</button>
+        </div>
+      </div>
+
+    </div>
+      
     </>
   )
 }
