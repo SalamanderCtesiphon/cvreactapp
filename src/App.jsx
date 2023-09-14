2a18ab74aaff8f8364b3e8cdbf29699f844428a6
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-14 17:54:49 -0500

    tested out an input element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [firstName, setFirstName] = useState('')
 
   return (
     <>
-    
+    <input 
+      type="text" 
+      name="firstName" 
+      id="firstName" 
+      value={firstName}
+      placeholder='enter your first name'
+      onChange={(e) => setFirstName(e.target.value)}
+    />
+    {firstName}
     </>
   )
 }
